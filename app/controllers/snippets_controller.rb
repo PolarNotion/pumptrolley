3d15846070f1a3fc97b7cfc177accf35c9c88ace
Morgan Lopes <morganjlopes@morgans-mbp.attlocal.net>
2016-02-01 19:05:00 -0500

    Rmove restriction on snippet show.

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,5 +1,5 @@
 class SnippetsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :set_snippet, only: [:show, :edit, :update, :destroy]
 
   # GET /snippets
