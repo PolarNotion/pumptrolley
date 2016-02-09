63f0882b391fb034b8add3f0399723ff9ee2e3a1
Morgan Lopes <morganjlopes@morgans-mbp.attlocal.net>
2016-02-08 22:19:14 -0500

    Refactor home page.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Runonrails
   class Application < Rails::Application
     Mime::Type.register "text/ruby", :rb
+    config.assets.precompile += %w( screenshare.gif )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
